5270e9050ae38a31d275be9b75b7d9d3721738be
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2022-08-21 07:08:42 +0600

    solved number: 47

---
+++ Problem-Solves/Fundamentals/problem47.js
@@ -0,0 +1,5 @@
+const greaterThanLessThan = (a, b, c) => a < b < c;
+
+console.log(greaterThanLessThan(1, 2, 3));
+console.log(greaterThanLessThan(3, 2, 1));
+console.log(greaterThanLessThan(-3, -2, -1));
